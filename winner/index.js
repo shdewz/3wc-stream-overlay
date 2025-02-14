8c87041159afca3fd44e5cf58dfc80df8618ad80
shdewz <shdewz@gmail.com>
2025-02-14 17:31:34 +0200

    Update winner scene

--- winner/index.js
+++ winner/index.js
@@ -1,51 +1,38 @@
-let teams = null;
-let mappool = null;
-let points_r, points_b;
+let comingup, teams, mappool;
 (async () => {
 	$.ajaxSetup({ cache: false });
+	comingup = await $.getJSON('../_data/coming_up.json');
 	teams = await $.getJSON('../_data/teams.json');
 	mappool = await $.getJSON('../_data/beatmaps.json');
-	document.getElementById('stage-name').innerHTML = mappool.stage.toUpperCase();
+	$('#stage').text(mappool.stage);
 })();
 
-let socket = new ReconnectingWebSocket('ws://' + location.host + '/ws');
+const cache = {};
 
-let title = document.getElementById('title');
-
+const socket = new ReconnectingWebSocket(`ws://${location.host}/websocket/v2`);
 socket.onopen = () => { console.log('Successfully Connected'); };
 socket.onclose = event => { console.log('Socket Closed Connection: ', event); socket.send('Client Closed!'); };
 socket.onerror = error => { console.log('Socket Error: ', error); };
 
-let tempTime, tempMapName;
+const update_team = (color, team) => {
+	$(`#name_${color}`).text(team.team);
+	$(`#flag_${color}`).css('background-image', `url('../_shared/assets/flags/${team.flag}.png')`);
+};
 
-socket.onmessage = event => {
-	let data = JSON.parse(event.data);
+socket.onmessage = async event => {
+	const data = JSON.parse(event.data);
 
-	if (tempMapName !== `${data.menu.bm.metadata.artist} - <b>${data.menu.bm.metadata.title}</b>`) {
-		tempMapName = `${data.menu.bm.metadata.artist} - <b>${data.menu.bm.metadata.title}</b>`;
-		title.innerHTML = `<span id="note">♪</span> ${tempMapName}`;
-	}
+	if (teams && (cache.points_r !== data.tourney.points.left || cache.points_b !== data.tourney.points.right)) {
+		cache.points_r = data.tourney.points.left;
+		cache.points_b = data.tourney.points.right;
+		const red_team = teams.find(team => team.team === data.tourney.team.left);
+		const blue_team = teams.find(team => team.team === data.tourney.team.right);
 
-	if (teams && (points_r !== data.tourney.manager.stars.left || points_b !== data.tourney.manager.stars.right)) {
-		points_r = data.tourney.manager.stars.left;
-		points_b = data.tourney.manager.stars.right;
-		let red = teams.find(t => t.name === data.tourney.manager.teamName.left);
-		let blue = teams.find(t => t.name === data.tourney.manager.teamName.right);
-
-		if (red && blue) {
-			document.getElementById('red-team').innerHTML = red.name;
-			document.getElementById('red-flag').src = `https://assets.ppy.sh/old-flags/${red.flag}.png`;
-			document.getElementById('blue-team').innerHTML = blue.name;
-			document.getElementById('blue-flag').src = `https://assets.ppy.sh/old-flags/${blue.flag}.png`;
-			document.getElementById('score').innerHTML = `${points_r} – ${points_b}`;
-			for (let i = 0; i < 6; i++) {
-				let player = red.players[i]?.username || '';
-				document.getElementById(`red-player-${i + 1}`).innerHTML = player;
-			}
-			for (let i = 0; i < 6; i++) {
-				let player = blue.players[i]?.username || '';
-				document.getElementById(`blue-player-${i + 1}`).innerHTML = player;
-			}
+		if (red_team && blue_team) {
+			update_team('red', red_team);
+			update_team('blue', blue_team);
+			$('#score_red').text([cache.points_r]);
+			$('#score_blue').text([cache.points_b]);
 		}
 	}
 }

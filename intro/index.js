e09e1dffc1f0819f89910e531fbeb16b2bbefe05
shdewz <shdewz@gmail.com>
2025-02-14 17:31:07 +0200

    Update intro scene

--- intro/index.js
+++ intro/index.js
@@ -4,67 +4,62 @@ let comingup, teams, mappool;
 	comingup = await $.getJSON('../_data/coming_up.json');
 	teams = await $.getJSON('../_data/teams.json');
 	mappool = await $.getJSON('../_data/beatmaps.json');
-	document.getElementById('stage-name').innerHTML = mappool.stage.toUpperCase();
+	const streamer = (await $.getJSON('../_data/streamer.json')).username;
 
-	let timer_end = comingup.time - 0 * 60 * 60 * 1000;
-	if (timer_end > Date.now()) {
-		let timer_int = setInterval(() => {
-			if (timer_end < Date.now()) {
-				clearInterval(timer_int);
-				if (timer) timer.innerHTML = '00:00';
+	if (comingup.length) {
+		const now = Date.now();
+		const matches = comingup.sort((a, b) => a.time - b.time).filter(e => e.time > now - 3 * 60 * 1000);
+		if (matches.length === 0) return;
+
+		concurrent_matches = matches.filter(m => m.time == matches[0].time);
+		if (concurrent_matches.length > 1) {
+			concurrent_matches_2 = concurrent_matches.filter(m => m.streamer === streamer ?? '');
+			if (concurrent_matches_2.length === 0) {
+				update_match(concurrent_matches[0]);
 			}
-			let remaining = Math.floor((timer_end - Date.now()) / 1000);
-			let hours = Math.floor(remaining / 60 / 60);
-			let date = new Date(null);
-			date.setSeconds(remaining);
-			let text = hours > 0 ? date.toISOString().slice(11, 19) : date.toISOString().slice(14, 19);
-			if (timer && remaining > 0) timer.innerHTML = text;
-		}, 1000);
-	}
-
-	let team_red = teams.find(t => t.name == comingup.red_team);
-	if (team_red) {
-		document.getElementById('red-team').innerHTML = team_red.name;
-		document.getElementById('red-flag').src = `https://assets.ppy.sh/old-flags/${team_red.flag}.png`;
-		for (let i = 0; i < 6; i++) {
-			let player = team_red.players[i]?.username || '';
-			document.getElementById(`red-player-${i + 1}`).innerHTML = player;
+			else if (concurrent_matches_2.length > 1) {
+				console.log('you done fucked up');
+				$('#starting_title').text('MULTIPLE CONCURRENT MATCHES, CHECK JSON FILE');
+			}
+			else update_match(concurrent_matches_2[0]);
 		}
+		else update_match(concurrent_matches[0]);
 	}
-
-	let team_blue = teams.find(t => t.name == comingup.blue_team);
-	if (team_blue) {
-		document.getElementById('blue-team').innerHTML = team_blue.name;
-		document.getElementById('blue-flag').src = `https://assets.ppy.sh/old-flags/${team_blue.flag}.png`;
-		for (let i = 0; i < 6; i++) {
-			let player = team_blue.players[i]?.username || '';
-			document.getElementById(`blue-player-${i + 1}`).innerHTML = player;
-		}
-	}
+	else update_match(comingup);
 })();
 
-let socket = new ReconnectingWebSocket('ws://' + location.host + '/ws');
-
-let title = document.getElementById('title');
-let time = document.getElementById('time');
-let timer = document.getElementById('timer');
-
-socket.onopen = () => { console.log('Successfully Connected'); };
-socket.onclose = event => { console.log('Socket Closed Connection: ', event); socket.send('Client Closed!'); };
-socket.onerror = error => { console.log('Socket Error: ', error); };
-
-let tempTime, tempMapName;
-
-socket.onmessage = event => {
-	let data = JSON.parse(event.data);
-
-	if (tempMapName !== `${data.menu.bm.metadata.artist} - <b>${data.menu.bm.metadata.title}</b>`) {
-		tempMapName = `${data.menu.bm.metadata.artist} - <b>${data.menu.bm.metadata.title}</b>`;
-		title.innerHTML = `<span id="note">♪</span> ${tempMapName}`;
+const update_match = match => {
+	if (match.showcase) {
+		$('#match').css('display', 'none');
+		$('#starting_title').text(`${mappool.stage} Mappool Showcase`);
+	}
+	else {
+		$('#match').css('display', 'flex');
+		$('#starting_title').text(`${mappool.stage}:`);
+		const red_team = teams.find(team => team.team === match.red_team);
+		const blue_team = teams.find(team => team.team === match.blue_team);
+		update_team('red', red_team);
+		update_team('blue', blue_team);
 	}
 
-	// if (comingup && tempTime !== comingup.time) {
-	// 	tempTime = comingup.time;
-	// 	time.innerHTML = `${new Date(tempTime).toUTCString().substring(17, 22)} UTC`;
+	// maybe re-add this later
+	// if (match.time > Date.now()) {
+	// 	let timer_int = setInterval(() => {
+	// 		if (match.time < Date.now()) {
+	// 			clearInterval(timer_int);
+	// 			$('#timer').text('00:00');
+	// 		}
+	// 		let remaining = Math.floor((match.time - Date.now()) / 1000);
+	// 		let hours = Math.floor(remaining / 60 / 60);
+	// 		let date = new Date(null);
+	// 		date.setSeconds(remaining);
+	// 		let text = hours > 0 ? date.toISOString().slice(11, 19) : date.toISOString().slice(14, 19);
+	// 		if (timer && remaining > 0) $('#timer').text(text);
+	// 	}, 1000);
 	// }
-}
+};
+
+const update_team = (color, team) => {
+	$(`#name_${color}`).text(team.team);
+	$(`#flag_${color}`).css('background-image', `url('../_shared/assets/flags/${team.flag}.png')`);
+};
